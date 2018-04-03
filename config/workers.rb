47c05f569d8d2e6ccd20ff572aaeab6a1805ac46
Josh Holtz <josh@rokkincat.com>
2018-04-03 11:33:07 -0500

    Concurrency man

--- config/workers.rb
+++ config/workers.rb
@@ -5,6 +5,8 @@ Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_URL') }
 end
 
+Sidekiq.options[:concurrency] = 1
+
 Sidekiq.configure_client do |config|
   config.redis = { :size => ENV.fetch('REDIS_CONNECTIONS_CLIENT').to_i }
 end
