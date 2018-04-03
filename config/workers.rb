465337099d2f8c9b69ac329064d4345c53627ef2
Josh Holtz <josh@rokkincat.com>
2018-04-03 11:22:23 -0500

    eyes are important

--- config/workers.rb
+++ config/workers.rb
@@ -6,11 +6,11 @@ Sidekiq.configure_client do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :size => ENV.fetch('REDIS_CONNECTIONS') }
+  config.redis = { :size => ENV.fetch('REDIS_CONNECTIONS').to_i }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :size => ENV.fetch('REDIS_CONNECTIONS') }
+  config.redis = { :size => ENV.fetch('REDIS_CONNECTIONS').to_i }
 end
 
 Dir[File.expand_path('../../workers/*.rb', __FILE__)].each { |file|require file }
